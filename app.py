0f210f8bae1729b075bb13287bd369c0639cfbc7
Azadova Indira <aindira2197@gmail.com>
2026-04-07 21:01:15 +0500

    Create app.py

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+mahsulotlar = ["Noutbuk", "Telefon", "Planshet", "Quloqchin", "Kamera"]
+
+@app.route("/mahsulot/<int:indeks>")
+def mahsulot_detail(indeks):
+    if 0 <= indeks < len(mahsulotlar):
+        return render_template("detail.html", mahsulot=mahsulotlar[indeks])
+    else:
+        return render_template("detail.html", mahsulot=None)
+
+if __name__ == "__main__":
+    app.run(debug=True)
